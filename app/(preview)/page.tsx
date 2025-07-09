948e54c64cf50562a2c861ba6e6fa467b9457ee2
Woody Wu <wwu@smceu.com>
2025-07-09 13:55:56 +0100

    fix

--- app/(preview)/page.tsx
+++ app/(preview)/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from "react";
 
-type ReportType = "spoofing" | "multi" | "software" | "other";
+type ReportType = "spoofing" | "multi" | "software" | "item" | "other";
 
 const reportTypeMap = {
   spoofing: {
